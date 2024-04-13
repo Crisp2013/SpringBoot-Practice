cbe8611c01b3fcc72b2a7618e22a64f5b39ef18e
WindowsTiger <0xodbs02@naver.com>
2024-04-13 22:39:12 +0900

    feat: Add template page

---
+++ mysite/src/main/java/com/practice/mysite/question/QuestionController.java
@@ -0,0 +1,24 @@
+package com.practice.mysite.question;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor //final이 붙은 생성자를 자동으로 만들어줌->questionRepository에 자동으로 모델 주입
+@Controller
+public class QuestionController {
+    
+    private final QuestionRepository questionRepository;
+    
+    @GetMapping("/question/list")
+    public String list(Model model){//모델객체는 알아서 생김, 탬플릿에 전달해주는 것, django에서는 context
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList",questionList);
+        return "question_list";
+    }
+    
+}

be0947cb3dbb6695c51cc79495880338d94f09ea
WindowsTiger <0xodbs02@naver.com>
2024-04-14 17:31:49 +0900

    feat: Add root site

--- mysite/src/main/java/com/practice/mysite/MainController.java
+++ mysite/src/main/java/com/practice/mysite/MainController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
+    @GetMapping("/")
+    public String root(){
+        return "redirect:/question/list";
+    }
     @GetMapping("/test")
     @ResponseBody
     public String index(){

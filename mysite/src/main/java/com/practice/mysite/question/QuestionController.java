876b9372d525f8b65daf48a1058044912418f60b
WindowsTiger <0xodbs02@naver.com>
2024-04-14 17:43:20 +0900

    style: Add URL prefix

--- mysite/src/main/java/com/practice/mysite/question/QuestionController.java
+++ mysite/src/main/java/com/practice/mysite/question/QuestionController.java
@@ -6,22 +6,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
-
+@RequestMapping("/question")
 @RequiredArgsConstructor //final이 붙은 생성자를 자동으로 만들어줌->questionRepository에 자동으로 모델 주입
 @Controller
 public class QuestionController {
     
     private final QuestionService questionService;
     
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model){//모델객체는 알아서 생김, 탬플릿에 전달해주는 것, django에서는 context
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList",questionList);
         return "question_list";
     }
-    @GetMapping("question/detail/{id}")
+    @GetMapping("/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id){
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question",question);

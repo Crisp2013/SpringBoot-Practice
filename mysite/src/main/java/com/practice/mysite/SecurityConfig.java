ed17b0e90ef9a0938104e99d0af837d653437787
WindowsTiger <0xodbs02@naver.com>
2024-04-24 14:15:07 +0900

    chore: Add spring security dependencies

---
+++ mysite/src/main/java/com/practice/mysite/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.practice.mysite;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity //스프링 시큐리티의 제어를 받음
+public class SecurityConfig {
+    @Bean //스프링에 의해 생성 또는 관리되는 객체
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+            .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+            //h2 콘솔 csrf 검사 제외
+            .csrf((csrf) -> csrf
+                .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+            //같은 사이트의 iframe 허용
+            .headers((headers) -> headers
+                .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                    XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+            )
+        ;
+        return http.build();
+    }
+}
